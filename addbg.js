9b7f064dbcceffda7900eed023aa4f9a80d2b452
Klaus Waiss <klaus.waiss@gmx.at>
2016-05-01 17:49:03 +0200

    - Fixed.

--- addbg.js
+++ addbg.js
@@ -1,14 +1,20 @@
 chrome.storage.sync.get({
-		bggecbgimage: false
-		}, function(items) {
+	bggecbgimage: false
+	}, function(items) {
 
-			if (!items.bggecbgimage) {		
-		
-				var gamePrimaryDivs = document.getElementsByClassName("game-primary");
-				for(var i = 0; i < gamePrimaryDivs.length; i++) {
-					var item = gamePrimaryDivs.item(i)
+		if (!items.bggecbgimage) {		
+	
+			var gamePrimaryDivs = document.getElementsByClassName("game-primary");
+			for(var i = 0; i < gamePrimaryDivs.length; i++) {
+				var item = gamePrimaryDivs.item(i)
 					item.style["background-image"]='none';
 					item.style["background-color"]='transparent';
-				}
 			}
-	});
+		}
+		
+		var gamePrimaryDivs = document.getElementsByClassName("game-primary");
+		for(var i = 0; i < gamePrimaryDivs.length; i++) {
+			var item = gamePrimaryDivs.item(i)
+			item.style["visibility"]="visible";
+		}
+});
