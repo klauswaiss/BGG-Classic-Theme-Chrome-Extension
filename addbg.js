a942b51b5aa606fdcf1b05806a7c811e77db26e4
Klaus Waiss <klaus.waiss@gmx.at>
2016-05-01 16:35:04 +0200

    - Fixed checkbox (was inverse for showing/hiding background image).

--- addbg.js
+++ addbg.js
@@ -2,7 +2,7 @@ chrome.storage.sync.get({
 		bggecbgimage: false
 		}, function(items) {
 
-			if (items.bggecbgimage) {		
+			if (!items.bggecbgimage) {		
 		
 				var gamePrimaryDivs = document.getElementsByClassName("game-primary");
 				for(var i = 0; i < gamePrimaryDivs.length; i++) {
